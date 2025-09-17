cb9aa8b408407a9683436c3c1dc6a533d5830319
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-17 10:20:21 +0900

    [Feat] chapter 2-03 JPA로 데이터베이스 사용하기

--- src/main/java/com/example/HelloLombok.java
+++ src/main/java/com/example/HelloLombok.java
@@ -9,9 +9,4 @@ public class HelloLombok {
     private final String hello; 
     private final int lombok; 
 
-    public static void main(String[] args) { 
-        HelloLombok helloLombok = new HelloLombok("헬로", 5); 
-        System.out.println(helloLombok.getHello()); 
-        System.out.println(helloLombok.getLombok()); 
-    } 
 }

c42f78c13a733b22f34b3cb26a6191ec3ffdcb65
gunha0405 <dhrjsgk0405@gmail.com>
2025-09-17 14:43:09 +0900

    [Feat] chapter 3-10 추천 기능

--- src/main/java/com/example/question/Question.java
+++ src/main/java/com/example/question/Question.java
@@ -2,6 +2,7 @@ package com.example.question;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import com.example.answer.Answer;
 import com.example.user.SiteUser;
@@ -12,6 +13,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
@@ -41,4 +43,7 @@ public class Question {
     
     @ManyToOne
     private SiteUser author;
+    
+    @ManyToMany
+    Set<SiteUser> voter;
 }
